fa38cb0252af83a17cc21f00ae74f70783450f87
UGURINAL <ugurinal01@hotmail.com>
2021-04-02 12:06:05 +0300

    2D Platformer
    +added: scene loader
    +updated: ui and build settings
    +fix: some minor bug fixes
    
    final commit...

---
+++ Assets/Scripts/SceneController/SceneLoader.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+namespace Platformer2D.Utility
+{
+    public class SceneLoader : MonoBehaviour
+    {
+        public void ChangeScene()
+        {
+            int buildIndex = SceneManager.GetActiveScene().buildIndex;
+
+            if (buildIndex == 0)
+                SceneManager.LoadScene(1);
+            else
+                SceneManager.LoadScene(0);
+        }
+    }
+}
